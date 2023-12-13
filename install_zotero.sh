95366746ac2cf08603d1d311d2df3443fe8ed383
JohannesWiesner <joh.wiesner@gmail.com>
2023-12-13 15:57:51 +0100

    added chmod -R 707 so zotero can update automatically

--- install_zotero.sh
+++ install_zotero.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
+# FIXME: User has to manually download .tar.bz file first, but it would be nice if we could automatize this
 
 # install zotero
 # https://wiki.ubuntuusers.de/Zotero/#Systemweite-Installation-fuer-alle-Benutzer
 
-# FIXME: User has to manually download .tar.bz file first,
-# but it would be nice if we could automatize this
-
 # extract .tar.bz file into /opt/zotero
 # we use a wildcard so it always works with the latest version
 # and we use strip components to get rid of the top-level directory
-sudo mkdir /opt/zotero
-sudo tar -xf ~/Downloads/Zotero-*_linux-x86_64.tar.bz2 --strip-components=1 -C /opt/zotero
-sudo bash /opt/zotero/set_launcher_icon
-sudo ln -s /opt/zotero/zotero.desktop /usr/share/applications/
+mkdir /opt/zotero
+tar -xf ~/Downloads/Zotero-*_linux-x86_64.tar.bz2 --strip-components=1 -C /opt/zotero
+bash /opt/zotero/set_launcher_icon
+ln -s /opt/zotero/zotero.desktop /usr/share/applications/
+
+# finally, we have to give others read, write and execute rights so zotero can update automatically
+chmod -R 707 /opt/zotero
 
 # we also want the default-jre and the libreoffice-java-common packages
 # so that we can use the libre office plugin
-sudo apt install default-jre
-sudo apt install libreoffice-java-common
+apt install default-jre
+apt install libreoffice-java-common
