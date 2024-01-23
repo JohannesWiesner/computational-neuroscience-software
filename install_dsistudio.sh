06151f2fbcc005ac7efdb543503c6615fac7cacc
JohannesWiesner <joh.wiesner@gmail.com>
2024-01-23 19:08:14 +0100

    added installation of libqt6charts-dev for dsistudio in order to prevent documented error (as described on dsi studio page + i could reproduce that error

--- install_dsistudio.sh
+++ install_dsistudio.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
 # install DSI-Studio
 # https://dsi-studio.labsolver.org/download.html
+
+# as described on the download page (in order to avoid this error):
+# ./dsi_studio: error while loading shared libraries: libQt6Charts.so.6: cannot open shared object file: No such file or directory
+# we install the following:
+apt install libqt6charts6-dev
+
 install_file=$(find ./deb_files -name 'dsi_studio*.zip')
 
 # check if the file exists and only then run installation command
