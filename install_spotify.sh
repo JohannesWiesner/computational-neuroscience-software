f08ba7b4b5ebf1f9282c32cbcd51a503d98e2f1b
JohannesWiesner <joh.wiesner@gmail.com>
2024-02-02 21:21:17 +0100

    don't put stuff in Downloads folder

--- install_spotify.sh
+++ install_spotify.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Install spotify client https://www.spotify.com/de/download/linux/
 
-# get current users home directory
-USER_HOME=$(eval echo ~${SUDO_USER})
-
-cd $USER_HOME/Downloads
 curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
 apt-get update && apt-get install spotify-client
