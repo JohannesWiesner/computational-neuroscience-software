91ec85c065dfc10486dfb73f84cedbd0f6d2a577
JohannesWiesner <joh.wiesner@gmail.com>
2024-02-02 21:20:37 +0100

    download to installation_files

--- install_obsidian.sh
+++ install_obsidian.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# get current users home directory
-USER_HOME=$(eval echo ~${SUDO_USER})
-
-# get the path to the latest obsidian .deb file
+# get the path to the latest obsidian .deb file on GitHub
 obsidian_url=$(wget -q -O - https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep 'deb"$' | awk -F'"' ' {print $4} ')
 
-# downlod the .deb file to Downloads folder
-curl -L $obsidian_url -o $USER_HOME/Downloads/obsidian_latest.deb
+# downlod the .deb file to the installation files folder
+curl -L $obsidian_url -o ./installation_files/obsidian_latest.deb
 
 # install obsidian using the .deb file
-apt install $USER_HOME/Downloads/obsidian_latest.deb && rm $USER_HOME/Downloads/obsidian_latest.deb
+apt install ./installation_files/obsidian_latest.deb
