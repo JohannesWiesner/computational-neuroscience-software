bd404f8c4ae2d5c6f2b497c45e0cb8d2b4247b3a
JohannesWiesner <joh.wiesner@gmail.com>
2024-02-02 21:10:19 +0100

    removed sudo commands from this script as the script itself should be invoked with sudo

--- install_micromamba.sh
+++ install_micromamba.sh
@@ -1,10 +1,10 @@
 # the g++ compiler has to be installed because sometimes we have to compile packages from source if they are not available as pip-packages
 # for the desired python version
-sudo apt install g++
+apt install g++
 
 # install micromamba
-# https://mamba.readthedocs.io/en/latest/micromamba-installation.html#manual-installation
-sudo mkdir /opt/micromamba & cd /opt/micromamba
+# https://mamba.readthedocs.io/en/latest/installation/micromamba-installation.html#manual-installation
+mkdir /opt/micromamba & cd /opt/micromamba
 curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 export MAMBA_ROOT_PREFIX=~/micromamba  # optional, defaults to ~/micromamba
 eval "$(./bin/micromamba shell hook -s posix)"
