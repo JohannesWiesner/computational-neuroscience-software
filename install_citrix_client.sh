0c09bd3e53583a83e208cbe08e00a8801d23561c
Johannes Wiesner <johannes.wiesner@pop-os.fritz.box>
2024-04-09 22:55:34 +0200

    added script to instal citrix workspace client

---
+++ install_citrix_client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install Ferdium
+# https://ferdium.org/download
+deb_file=$(find ./installation_files -name 'icaclient*.deb')
+
+# check if the file exists and only then run installation command
+# https://unix.stackexchange.com/a/657087/540273
+if [[ -n $deb_file ]]
+then
+  apt -y install $deb_file
+else
+  echo "Could not find .deb file for Citrix-Client,, please download first and put in installation_files folder"
+fi
